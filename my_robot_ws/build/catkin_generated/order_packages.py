43cdd197dffd33a6d09ceb521a263c3b112ded6c
Seif <seif.r.gh@gmail.com>
2021-05-30 12:26:25 +0200

    gg

---
+++ my_robot_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/seif/my_robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/seif/my_robot_ws/devel;/opt/ros/melodic'.split(';') if '/home/seif/my_robot_ws/devel;/opt/ros/melodic' != '' else []
---
+++ my_robot_ws/build/chatter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seif/my_robot_ws/src/chatter
+# Build directory: /home/seif/my_robot_ws/build/chatter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
